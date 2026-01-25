c3a1d0824ffe228a45611fc85d411ea68fdff6ec
madhavpandey1478-sys <madhavpandey1478@gmail.com>
2026-01-25 20:43:57 +0530

    Update excel (1) 1.js

--- excel (1) 1.js
+++ excel (1) 1.js
@@ -135,7 +135,7 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             var that = this;
 
-            let xlsxjs = "https://priyankayala.github.io/ExcelUpload/xlsx.js";
+            let xlsxjs = "https://madhavpandey1478-sys.github.io/vigilant-octo-fiesta/xlsx (1) 1.js";
             async function LoadLibs() {
                 try {
                     await loadScript(xlsxjs, _shadowRoot);
