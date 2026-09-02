3f33fcce01dfdd7d2a8c806bed290a67ce019d5b
madhavpandey1478-sys <madhavpandey1478@gmail.com>
2026-09-02 10:08:26 +0530

    Update aps_excel.js

--- aps_excel.js
+++ aps_excel.js
@@ -1,462 +1,165 @@
 (function () {
-
     let tmpl = document.createElement("template");
-
     tmpl.innerHTML = `
-        <style>
-
-            fieldset {
-                margin-bottom: 10px;
-                border: 1px solid #afafaf;
-                border-radius: 3px;
-            }
-
-            table {
-                width: 100%;
-            }
-
-            input,
-            textarea,
-            select {
-                font-family: "72", Arial, Helvetica, sans-serif;
-                width: 100%;
-                padding: 4px;
-                box-sizing: border-box;
-                border: 1px solid #bfbfbf;
-            }
-
-            input[type=checkbox] {
-                width: inherit;
-                margin: 6px 3px 6px 0;
-                vertical-align: middle;
-            }
-
-        </style>
-
-        <form id="form" autocomplete="off">
-
-            <fieldset>
-
-                <legend>General</legend>
-
-                <table>
-
-                    <tr>
-                        <td>
-                            <label for="title">Title</label>
-                        </td>
-
-                        <td>
-                            <input
-                                id="title"
-                                name="title"
-                                type="text">
-                        </td>
-                    </tr>
-
-
-                    <tr>
-                        <td>
-                            <label for="subtitle">Sub Title</label>
-                        </td>
-
-                        <td>
-                            <input
-                                id="subtitle"
-                                name="subtitle"
-                                type="text">
-                        </td>
-                    </tr>
-
-
-                    <tr>
-                        <td>
-                            <label for="icon">Icon</label>
-                        </td>
-
-                        <td>
-                            <input
-                                id="icon"
-                                name="icon"
-                                type="text">
-                        </td>
-                    </tr>
-
-
-                    <tr>
-                        <td>
-                            <label for="unit">Unit</label>
-                        </td>
-
-                        <td>
-                            <input
-                                id="unit"
-                                name="unit"
-                                type="text">
-                        </td>
-                    </tr>
-
-
-                    <tr>
-                        <td>
-                            <label for="footer">Footer</label>
-                        </td>
-
-                        <td>
-                            <input
-                                id="footer"
-                                name="footer"
-                                type="text">
-                        </td>
-                    </tr>
-
-                </table>
-
-            </fieldset>
-
-            <button
-                type="submit"
-                hidden>
-                Submit
-            </button>
-
-        </form>
+      <style>
+          fieldset {
+              margin-bottom: 10px;
+              border: 1px solid #afafaf;
+              border-radius: 3px;
+          }
+          table {
+              width: 100%;
+          }
+          input, textarea, select {
+              font-family: "72",Arial,Helvetica,sans-serif;
+              width: 100%;
+              padding: 4px;
+              box-sizing: border-box;
+              border: 1px solid #bfbfbf;
+          }
+          input[type=checkbox] {
+              width: inherit;
+              margin: 6px 3px 6px 0;
+              vertical-align: middle;
+          }
+      </style>
+      <form id="form" autocomplete="off">
+        <fieldset> 
+          <legend>General</legend>
+          <table>
+            <tr>
+              <td><label for="Title">Title</label></td>
+              <td><input id="title" name="title" type="text"></td>
+            </tr>
+            <tr>
+              <td><label for="Sub Title">Sub Title</label></td>
+              <td><input id="subtitle" name="subtitle" type="text"></td>
+            </tr>
+            <tr>
+              <td><label for="Icon">Icon</label></td>
+              <td><input id="icon" name="icon" type="text"></td>
+            </tr>
+            <tr>
+              <td><label for="Unit">Unit</label></td>
+              <td><input id="unit" name="unit" type="text"></td>
+            </tr>
+            <tr>
+              <td><label for="Footer">Footer</label></td>
+              <td><input id="footer" name="footer" type="text"></td>
+            </tr>
+          </table>
+        </fieldset>
+        <button type="submit" hidden>Submit</button>
+      </form>
     `;
 
-
     class ExcelAps extends HTMLElement {
-
         constructor() {
-
             super();
+            this._shadowRoot = this.attachShadow({ mode: "open" });
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 
-
-            /* Create Shadow DOM */
-
-            this._shadowRoot =
-                this.attachShadow({
-                    mode: "open"
-                });
-
-
-            /* Add template */
-
-            this._shadowRoot.appendChild(
-                tmpl.content.cloneNode(true)
-            );
-
-
-            /* Get form */
-
-            let form =
-                this._shadowRoot.getElementById(
-                    "form"
-                );
-
-
-            /* Form events */
-
-            form.addEventListener(
-                "submit",
-                this._submit.bind(this)
-            );
-
-            form.addEventListener(
-                "change",
-                this._change.bind(this)
-            );
-
+            let form = this._shadowRoot.getElementById("form");
+            form.addEventListener("submit", this._submit.bind(this));
+            form.addEventListener("change", this._change.bind(this));
         }
-
 
         connectedCallback() {
-
         }
-
-
-        /* ==========================================
-           SUBMIT
-           ========================================== */
 
         _submit(e) {
-
             e.preventDefault();
-
-
             let properties = {};
-
-
-            for (
-                let name of ExcelAps.observedAttributes
-            ) {
-
-                properties[name] =
-                    this[name];
-
+            for (let name of ExcelAps.observedAttributes) {
+                properties[name] = this[name];
             }
-
-
-            this._firePropertiesChanged(
-                properties
-            );
-
-
+            this._firePropertiesChanged(properties);
             return false;
-
         }
-
-
-        /* ==========================================
-           CHANGE
-           ========================================== */
 
         _change(e) {
-
-            this._changeProperty(
-                e.target.name
-            );
-
+            this._changeProperty(e.target.name);
         }
-
 
         _changeProperty(name) {
-
             let properties = {};
-
-
-            properties[name] =
-                this[name];
-
-
-            this._firePropertiesChanged(
-                properties
-            );
-
+            properties[name] = this[name];
+            this._firePropertiesChanged(properties);
         }
 
-
-        /* ==========================================
-           FIRE PROPERTY CHANGED EVENT
-           ========================================== */
-
-        _firePropertiesChanged(
-            properties
-        ) {
-
-            this.dispatchEvent(
-                new CustomEvent(
-                    "propertiesChanged",
-                    {
-                        detail: {
-                            properties:
-                                properties
-                        }
-                    }
-                )
-            );
-
+        _firePropertiesChanged(properties) {
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: properties
+                }
+            }));
         }
-
-
-        /* ==========================================
-           TITLE
-           ========================================== */
 
         get title() {
-
-            return this.getValue(
-                "title"
-            );
-
+            return this.getValue("title");
         }
-
 
         set title(value) {
-
-            this.setValue(
-                "title",
-                value
-            );
-
+            this.setValue("title", value);
         }
-
-
-        /* ==========================================
-           SUBTITLE
-           ========================================== */
 
         get subtitle() {
-
-            console.log(
-                this.getValue("subtitle")
-            );
-
-
-            return this.getValue(
-                "subtitle"
-            );
-
+            console.log(this.getValue("subtitle"));
+            return this.getValue("subtitle");
         }
-
 
         set subtitle(value) {
-
-            this.setValue(
-                "subtitle",
-                value
-            );
-
+            this.setValue("subtitle", value);
         }
-
-
-        /* ==========================================
-           ICON
-           ========================================== */
 
         get icon() {
-
-            return this.getValue(
-                "icon"
-            );
-
+            return this.getValue("icon");
         }
-
 
         set icon(value) {
-
-            this.setValue(
-                "icon",
-                value
-            );
-
+            this.setValue("icon", value);
         }
-
-
-        /* ==========================================
-           UNIT
-           ========================================== */
 
         get unit() {
-
-            return this.getValue(
-                "unit"
-            );
-
+            return this.getValue("unit");
         }
-
 
         set unit(value) {
-
-            this.setValue(
-                "unit",
-                value
-            );
-
+            this.setValue("unit", value);
         }
-
-
-        /* ==========================================
-           FOOTER
-           ========================================== */
 
         get footer() {
-
-            return this.getValue(
-                "footer"
-            );
-
+            return this.getValue("footer");
         }
-
 
         set footer(value) {
-
-            this.setValue(
-                "footer",
-                value
-            );
-
+            this.setValue("footer", value);
         }
-
-
-        /* ==========================================
-           GET VALUE
-           ========================================== */
 
         getValue(id) {
-
-            return this._shadowRoot
-                .getElementById(id)
-                .value;
-
+            return this._shadowRoot.getElementById(id).value;
         }
 
-
-        /* ==========================================
-           SET VALUE
-           ========================================== */
-
-        setValue(
-            id,
-            value
-        ) {
-
-            this._shadowRoot
-                .getElementById(id)
-                .value = value;
-
+        setValue(id, value) {
+            this._shadowRoot.getElementById(id).value = value;
         }
-
-
-        /* ==========================================
-           OBSERVED ATTRIBUTES
-           ========================================== */
 
         static get observedAttributes() {
-
             return [
-
                 "title",
                 "subtitle",
                 "icon",
                 "unit",
                 "footer"
-
             ];
-
         }
 
-
-        /* ==========================================
-           ATTRIBUTE CHANGED
-           ========================================== */
-
-        attributeChangedCallback(
-            name,
-            oldValue,
-            newValue
-        ) {
-
-            if (
-                oldValue != newValue
-            ) {
-
-                this[name] =
-                    newValue;
-
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (oldValue != newValue) {
+                this[name] = newValue;
             }
-
         }
-
     }
 
-
-    /* ==============================================
-       REGISTER CUSTOM WEB COMPONENT
-       ============================================== */
-
-    customElements.define(
-        "com-fd-djaja-sap-sac-excel-aps",
-        ExcelAps
-    );
-
+    customElements.define("com-fd-djaja-sap-sac-excel-aps", ExcelAps);
 })();
